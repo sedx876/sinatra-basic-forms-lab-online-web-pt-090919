d75718541b549e830094cc224b40e1e2b4366366
Sharon Watkins <swatkins876@gmail.com>
2019-12-07 14:47:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,17 @@ require_relative 'models/puppy'
 
 class App < Sinatra::Base
 
-  get '/' do
+  get "/" do
+    erb :index
+  end
+
+  get "/new" do
     erb :create_puppy
+  end
+
+  post "/puppy" do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
   end
 
 end
